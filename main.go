e1607636cd0e87fbbc74bdc9417823c6bcf7bd98
amdonov <donovan_aaron@bah.com>
2018-09-06 20:37:43 -0400

    Moved connector creation to main

--- main.go
+++ main.go
@@ -5,6 +5,16 @@ import (
 	"os"
 	"github.com/dexidp/dex/cmd"
 	"github.com/spf13/cobra"
+	"github.com/dexidp/dex/connector/authproxy"
+	"github.com/dexidp/dex/connector/github"
+	"github.com/dexidp/dex/connector/gitlab"
+	"github.com/dexidp/dex/connector/ldap"
+	"github.com/dexidp/dex/connector/linkedin"
+	"github.com/dexidp/dex/connector/microsoft"
+	"github.com/dexidp/dex/connector/mock"
+	"github.com/dexidp/dex/connector/oidc"
+	"github.com/dexidp/dex/connector/saml"
+	"github.com/dexidp/dex/server"
 )
 
 func commandRoot() *cobra.Command {
@@ -21,6 +31,20 @@ func commandRoot() *cobra.Command {
 }
 
 func main() {
+	server.ConnectorsConfig = map[string]func() server.ConnectorConfig{
+		"mockCallback": func() server.ConnectorConfig { return new(mock.CallbackConfig) },
+		"mockPassword": func() server.ConnectorConfig { return new(mock.PasswordConfig) },
+		"ldap":         func() server.ConnectorConfig { return new(ldap.Config) },
+		"github":       func() server.ConnectorConfig { return new(github.Config) },
+		"gitlab":       func() server.ConnectorConfig { return new(gitlab.Config) },
+		"oidc":         func() server.ConnectorConfig { return new(oidc.Config) },
+		"saml":         func() server.ConnectorConfig { return new(saml.Config) },
+		"authproxy":    func() server.ConnectorConfig { return new(authproxy.Config) },
+		"linkedin":     func() server.ConnectorConfig { return new(linkedin.Config) },
+		"microsoft":    func() server.ConnectorConfig { return new(microsoft.Config) },
+		// Keep around for backwards compatibility.
+		"samlExperimental": func() server.ConnectorConfig { return new(saml.Config) },
+	}
 	if err := commandRoot().Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err.Error())
 		os.Exit(2)
